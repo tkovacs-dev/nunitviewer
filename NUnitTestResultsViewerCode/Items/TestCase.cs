7c0fc1e93f44c37fb59673c017cbe426ab7ec6a7
Sid Shetye <sid@crypteron.com>
2016-11-17 20:23:45 -0800

    initial commit

---
+++ NUnitTestResultsViewerCode/Items/TestCase.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace NUnitTestResultsViewerCode.Items
+{
+  class TestCase : TestResult
+  {
+    /// <summary>
+    /// Initializes TestCase class.
+    /// </summary>
+    /// <param name="element"></param>
+    private TestCase( XElement element )
+      : base( element )
+    {
+    }
+
+    /// <summary>
+    /// Load test case.
+    /// </summary>
+    /// <param name="element"></param>
+    /// <returns></returns>
+    public static TestCase Load( XElement element )
+    {
+      return new TestCase( element );
+    }
+
+    /// <summary>
+    /// Test Case output message.
+    /// </summary>
+    public string Message
+    {
+      get
+      {
+        return readDescenadantNodeValue( "message" );
+      }
+    }
+
+    /// <summary>
+    /// Message stack trace.
+    /// </summary>
+    public string StackTrace
+    {
+      get
+      {
+        return readDescenadantNodeValue( "stack-trace" );
+      }
+    }
+
+    private string readDescenadantNodeValue( string nodename )
+    {
+      var node = _element.Descendants( nodename ).FirstOrDefault();
+      if( null == node )
+      {
+        return string.Empty;
+      }
+      return node.Value;
+    }
+
+    /// <summary>
+    /// Throw an InvalidOperationException. Test Cases can not have child elements.
+    /// </summary>
+    /// <param name="item"></param>
+    public override void Add( BaseItem item )
+    {
+      throw new InvalidOperationException( "Test Cases could not contains child elements." );
+    }
+  }
+}
---
+++ NUnitTestResultsViewerCode/Items/TestResult.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.Drawing;
+using NUnitTestResultsViewerCode.Properties;
+
+namespace NUnitTestResultsViewerCode.Items
+{
+  /// <summary>
+  /// Represent Test Results base class.
+  /// </summary>
+  abstract class TestResult : BaseItem
+  {
+    /// <summary>
+    /// Initializes class.
+    /// </summary>
+    /// <param name="element"></param>
+    protected TestResult( XElement element )
+      : base( element )
+    {
+    }
+
+    /// <summary>
+    /// Is test has been executed.
+    /// </summary>
+    public bool IsExecuted
+    {
+      get
+      {
+        return (bool)readAttribute<bool>( "executed" );
+      }
+    }
+
+    /// <summary>
+    /// Test Result.
+    /// </summary>
+    public TestResultsEnum TestResultValue
+    {
+      get
+      {
+        return (TestResultsEnum)readAttribute<TestResultsEnum>( "result" );
+      }
+    }
+
+    /// <summary>
+    /// Exucution time.
+    /// </summary>
+    public string Time
+    {
+      get
+      {
+        if( !IsExecuted )
+        {
+          return null;
+        }
+        return (string)readAttribute<string>( "time" );
+      }
+    }
+
+    /// <summary>
+    /// Count of asserts.
+    /// </summary>
+    public int? Asserts
+    {
+      get
+      {
+        if( !IsExecuted )
+        {
+          return null;
+        }
+        return (int)readAttribute<int>( "asserts" );
+      }
+    }
+
+    /// <summary>
+    /// Initializes with default value.
+    /// </summary>
+    protected override void intialize()
+    {
+      base.intialize();
+
+      switch( TestResultValue )
+      {
+        case TestResultsEnum.Success:
+          ImageKey = Defines.IMAGE_KEY_SUCCESS;
+          SelectedImageKey = ImageKey;
+          break;
+        case TestResultsEnum.Failure:
+        case TestResultsEnum.Error:
+        case TestResultsEnum.Invalid:
+          ImageKey = Defines.IMAGE_KEY_FAILURE;
+          SelectedImageKey = ImageKey;
+          break;
+        case TestResultsEnum.Skipped:
+          ImageKey = Defines.IMAGE_KEY_SKIPPED;
+          SelectedImageKey = ImageKey;
+          break;
+        case TestResultsEnum.Ignored:
+          ImageKey = Defines.IMAGE_KEY_IGNORED;
+          SelectedImageKey = ImageKey;
+          break;
+        case TestResultsEnum.Inconclusive:
+          ImageKey = Defines.IMAGE_KEY_INCONCLUSIVE;
+          SelectedImageKey = ImageKey;
+          break;
+
+      }
+    }
+  }
+}
